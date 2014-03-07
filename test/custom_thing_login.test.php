664a28f3ae5f4bb512ee540722892bfa0a7909fc
Ihor Marusyk <ih2502mk@gmail.com>
2014-03-07 11:47:01 -0500

    exercise

---
+++ test/custom_thing_login.test.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../vendor//function_mock.php';
+
+class CustomThingTest extends PHPUnit_Framework_TestCase
+{
+  public function customThingLoginPermissionTest() {
+    
+  }
+  
+  
+  public function customThingLoginInitTest() {
+    
+  }  
+}
+
+
+?>
